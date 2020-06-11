1f10440a6a4913a74018c983dac844ce646a3dad
Daniel Baia <danielbaia29@gmail.com>
2020-06-11 10:52:29 -0300

    new structure folder

---
+++ src/app/controllers/teachers.js
@@ -0,0 +1,13 @@
+const { age, date, degree } = require("../../lib/utils");
+
+
+module.exports = {
+
+    index(req, res) { return res.render('teachers/index') },
+    create(req, res) { return; },
+    post(req, res) { return; },
+    show(req, res) { return; },
+    edit(req, res) { return; },
+    put(req, res) { return; },
+    delete(req, res) { return; },
+}
